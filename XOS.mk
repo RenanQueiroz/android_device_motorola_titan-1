cb486809aa75fb1c7e3186701e30711d1de04dff
RenanQueiroz <queirozrrq@gmail.com>
2017-07-02 18:47:12 -0400

    Update XOS.mk

--- XOS.mk
+++ XOS.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/falcon/full_titan.mk)
 
-# Inherit some common HalogenOS stuff.
+# Inherit some common stuff.
 $(call inherit-product, vendor/xos/config/common.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 PRODUCT_RELEASE_NAME := MOTO G (2014)
-PRODUCT_NAME := viper_titan
+PRODUCT_NAME := XOS_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
